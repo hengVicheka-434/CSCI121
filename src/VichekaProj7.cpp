c265fc40e55187000aed6342dab4440ebb399c9a
heng.vicheka <159117729+hengVicheka-434@users.noreply.github.com>
2024-11-20 10:49:16 +0700

    Update VichekaProj7.cpp

--- src/VichekaProj7.cpp
+++ src/VichekaProj7.cpp
@@ -1,3 +1,5 @@
+void menu();
+
 void fill_array(int arr[], int size);
 // pre-condition: The arr has actual size that is greater than or equal to size
 // post-condition: arr[0], ..., arr[size-1] is filled from keyboard
@@ -16,6 +18,7 @@ void insert_sort(int arr[], int size);
 void bubble_sort(int arr[], int size);
 // pre-condition: arr has given size
 // post-condition: the elements in arr are rearranged from least to largest
+
 int main() {
   int choice;
   int a[9];
@@ -78,33 +81,98 @@ int main() {
 }
 
 // functions implementation
+void menu(){
+	cout << "
+}
+
 void fill_array(int arr[], int size){
-  
+  if (size >= sizeOf(arr){
+		cout << "Enter your sequence of values into the array: ";
+    for (int i = 0; i<size; i++){
+			cin >> arr[i];
+		}
+  } else {
+		return;
+	}
 }
 // pre-condition: The arr has actual size that is greater than or equal to size
 // post-condition: arr[0], ..., arr[size-1] is filled from keyboard
 void print_array(int arr[], int size){
-  
+  if (size >= sizeOf(arr){
+    for (int i = 0; i<size; i++){
+			cout << arr[i] << "\t";
+			if (i % 5 == 0)
+				cout << endl;
+		}
+  } else {
+		return;
+	}
 }
 // pre-condition: The arr has actual size that is greater than or equal to size
 // post-condition: arr[0], ..., arr[size-1] is printed to screen with 5 elements per line
 int linear_search(int arr[], int size, int key){
-  
+	int index;
+  if (size>0){
+		for (int i = 0; i<size; i++){
+			if (arr[i]==key)
+				index = i;
+		}
+		if ( index > size-1 )
+			return -1;
+		else 
+			return arr[index];
+	} 
 }
 // pre-condition: arr has given size
 // post-condition: The index of first occurrence of key in arr is returned. If the key cannot be found in arr, -1 is returned
 void select_sort(int arr[], int size){
-  
+	if (size > 0){
+		for (int i = 0; i < size - 1; i++) {
+			int minIndex = i;
+			for (int j = i + 1; j < size; j++) {
+				if (arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}
+			}
+			swap(arr[i], arr[minIndex]);
+    }
+	} else 
+		return;
 }
 // pre-condition: arr has given size 
 // post-condition: the elements in arr are rearranged from least to largest
 void insert_sort(int arr[], int size){
-  
+  if (size > 0){
+		for (int i = 1; i < size - 1; i++) {
+			int key = arr[i];
+			int j = i-1;
+
+			while (j>=0 && arr[j]>key){
+				arr[j+1] = arr[j];
+				j--;
+			}
+    }
+		arr[j+1] = key; 
+	} else 
+		return;
 }
 // pre-condition: arr has given size
 // post-condition: the elements in arr are rearranged from least to largest
 void bubble_sort(int arr[], int size){
-  
+  if (size > 0){
+		for (int i = 1; i < size - 1; i++) {
+			bool swapped = false;
+			for (int j = 0; j < size; j++){
+				if (arr[j] > arr[j+1]){
+					swap(arr[j], arr[j+1]);
+					swapped = true;
+				}
+			}
+			if (!swapped)
+				break;
+    }
+	} else 
+		return;
 }
 // pre-condition: arr has given size
 // post-condition: the elements in arr are rearranged from least to largest
